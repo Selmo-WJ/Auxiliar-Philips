c185e6546a95a50db7caa9c9cf2bc624fea9ce52
Selmo Werner Júnior <156967678+Selmo-W-Junior@users.noreply.github.com>
2025-01-23 23:28:43 -0300

    Start Project
    started the project, creating the screens and some page change actions.

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
+import function
 from tkinter import *
 
-def teste():
-    print('teste com funcao') 
-
-
 janela = Tk()
-janela.title("Sistema XML")
-texto_orientacao= Label(janela, text="teste")
-texto_orientacao.grid(column=0,row=0)
-texto_orientacao2= Label(janela, text="tefdfste")
-texto_orientacao2.grid(column=0,row=1)
 
-botao = Button(janela,text="teste aqui",command=teste)
-botao.grid(column=0,row=3)
+function.centralizar_janela(janela)
 
-texto_botao = Label(janela, )
+janela.title("Sistema XML para integração Padrão Tasy")
+
+# Label de orientação
+texto_orientacao = Label(janela, text="Selecione a Aplicação:")
+texto_orientacao.pack(side=TOP, fill=X, padx=0, pady=50)
+
+# Label vazio (para algum propósito específico, se necessário)
+texto_botao = Label(janela, text="")
+texto_botao.pack()
+
+# Botão Laboratório
+botaoLaboratorio = Button(janela, text="Laboratório", command=lambda: function.abrirJanelaLaboratorio())
+botaoLaboratorio.pack(pady=5)  # Separação entre os botões
+
+# Botão Não Laboratório
+botaoNaoLaboratorio = Button(janela, text="Não Laboratório", command=lambda: function.abrirJanelaNaoLaboratorio())
+botaoNaoLaboratorio.pack(pady=5)  # Separação leve entre os botões
+
+# Texto de rodapé com centralização
+texto_rodape = Label(janela, text="Criado por Selmo Werner Júnior", padx=20, pady=20)
+texto_rodape.place(relx=0.5, rely=1.0, anchor='s')
+
 janela.mainloop()
